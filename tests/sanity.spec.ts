91bd27e5855b4ea9cabf1cae1eca736ecde9493c
NivKotek <nivk99@gmail.com>
2025-03-15 23:03:30 +0200

    new

---
+++ tests/sanity.spec.ts
@@ -0,0 +1,17 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');ש
+  await page.locator('[data-test="login-credentials"]').click();
+  await page.locator('[data-test="username"]').click();
+  await page.locator('[data-test="username"]').click();
+  await page.locator('[data-test="username"]').fill('nk');
+  await page.locator('[data-test="password"]').click();
+  await page.locator('[data-test="password"]').fill('nk1234');
+  await page.locator('[data-test="login-button"]').click();
+  await page.locator('[data-test="username"]').dblclick();
+  await page.getByRole('heading', { name: 'Password for all users:' }).click();
+  await page.locator('[data-test="login-password"]').click();
+  await page.locator('[data-test="login-credentials"]').click();
+  await page.getByText('Swag Labs').click();
+});
